96c6fc9931b89dfb64d8166ce2fb33dc7c3f7b65
Simone Gaiarin <simgunz@gmail.com>
2021-09-18 12:37:24 +0200

    Imported keymap modifications from master

--- keychron-c1/keymaps/simgunz/keymap.c
+++ keychron-c1/keymaps/simgunz/keymap.c
@@ -43,49 +43,50 @@ enum layer_names {
 #define KC_SHIN LSFT(KC_INS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*  Windows layout
+    +--------------------------------------------------------------------------------------------+
+    | ESC |    | F1 | F2 | F3 | F4 |  | F5 | F6 | F7 | F8 | | F9 | F10| F11| F12| |PSCR|MIC |RGB |
+    +---------------------------------------------------------------------------+ +--------------+
+    |  ~  |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 |  - |  = | BACKSP  | |INS |HOME|PGUP|
+    +---------------------------------------------------------------------------+ +--------------+
+    |  TAB  |  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P |  [ |  ] |   \   | |DEL |END |PGDN|
+    +---------------------------------------------------------------------------+ +--------------+
+    | CAPSLCK  |  A |  S |  D |  F |  G |  H |  J |  K |  L | ; | ' |  RETURN   |                |
+    +---------------------------------------------------------------------------+      +----+    |
+    | LSHIFT     |  Z |  X |  C |  V |  B |  N |  M | , | . |  / |   RSHIFT     |      | UP |    |
+    +---------------------------------------------------------------------------+ +--------------+
+    |LCTRL| LWIN| LALT |            SPACE            | RALT| RWIN | FN | RCTRL  | |LFT |DWN |RGT |
+    +--------------------------------------------------------------------------------------------+
+    */
 
-
-/*
-				+--------------------------------------------------------------------------+----------------+
-				| ESC |    | F1 | F2 | F3 | F4 |  | F5 | F6 | F7 | F8 | | F9 | F10| F11| F12| |PSCR|SCRL|PAUS|
-				+---------------------------------------------------------------------------+------|----|----|
- 				|  ~  |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 |  - |  = | BACKSP  | |INS |HOME|PGUP|
- 				+---------------------------------------------------------------------------+------|----|----|
- 				|  TAB  |  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P |  [ |  ] |   \   | |DEL |END |PGDN|
- 				+---------------------------------------------------------------------------+------|----|----|
- 				| CAPSLCK  |  A |  S |  D |  F |  G |  H |  J |  K |  L | ; | ' |  RETURN   |                |
- 				+---------------------------------------------------------------------------+      |----|    |
- 				| LSHIFT     |  Z |  X |  C |  V |  B |  N |  M | , | . |  / |   RSHIFT     |      | UP |    |
- 				+---------------------------------------------------------------------------+------|----|----|
- 				|LCTRL| LWIN| LALT |            SPACE            | RALT| RWIN | FN | RCTRL  | |LFT |DWN |RGT |
- 				+---------------------------------------------------------------------------+----------------+
-*/
-    /*  Row:        0          1          2          3          4          5          6          7          8          9          10         11         12         13         14         15         16    */
-    [WIN_BASE] = {
-                {   KC_ESC,    KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_MPRV,   KC_MPLY,   KC_MNXT,   KC_MUTE,   KC_VOLD,   KC_VOLU,   KC_NO,     KC_PSCR,   KC_MIC,    RGB_TOG  },
+    [WIN_BASE] = { // Windows base layout
+                /*  0          1          2          3          4          5          6          7          8          9          10         11         12         13         14         15         16     */
+                {   KC_ESC,    KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_MPRV,   KC_MPLY,   KC_MNXT,   KC_MUTE,   KC_VOLD,   KC_VOLU,   KC_NO,     KC_PSCR,   KC_MIC,    RGB_TOG },
                 {   KC_GRV,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_MINS,   KC_EQL,    KC_BSPC,   KC_INS,    KC_HOME,   KC_PGUP },
                 {   KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_BSLS,   KC_DEL,    KC_END,    KC_PGDN },
                 {   KC_MOCP,   KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,   KC_NO,     KC_ENT,    KC_NO,     KC_NO,     KC_NO   },
                 {   KC_LSFT,   KC_NO,     KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_NO,     KC_RSFT,   KC_NO,     KC_UP,     KC_NO   },
                 {   KC_LCTL,   KC_LWIN,   KC_LALT,   KC_NO,     KC_NO,     KC_NO,     KC_SPC,    KC_NO,     KC_NO,     KC_NO,     KC_RALT,   KC_RWIN,   KC_MOFN,   KC_RCTL,   KC_LEFT,   KC_DOWN,   KC_RGHT }
             },
+
 /*
-				+--------------------------------------------------------------------------+----------------+
-				| RST |    |    |    |    |    |  |    |    |    |    | |    |    |    |    | |    |    |    |
-				+---------------------------------------------------------------------------+------|----|----|
- 				|     |    |    |    |    |    |    |    |    |    |    |    |    |         | |    |    |    |
- 				+---------------------------------------------------------------------------+------|----|----|
- 				|       |    |    |    |    |    |    |    |    |    |    |    |    |       | |    |    |    |
- 				+---------------------------------------------------------------------------+------|----|----|
- 				|          |    |    |    |    |    |    |    |    |    |   |   |           |                |
- 				+---------------------------------------------------------------------------+      |----|    |
- 				|            |    |    |    |    |    |    |    |   |   |    |              |      |    |    |
- 				+---------------------------------------------------------------------------+------|----|----|
- 				|     |     |      |                             |     |      |    |        | |    |    |    |
- 				+---------------------------------------------------------------------------+----------------+
+    +--------------------------------------------------------------------------+----------------+
+    |     |    |    |    |    |    |  |    |    |    |    | |    |    |    |    | |    |    |    |
+    +---------------------------------------------------------------------------+------|----|----|
+    |     |    |    |    |    |    |    |    |    |    |    |    |    |         | |    |    |    |
+    +---------------------------------------------------------------------------+------|----|----|
+    |       |    |    |    |    |    |    |    |    |    |    |    |    |       | |    |    |    |
+    +---------------------------------------------------------------------------+------|----|----|
+    |          |    |    |    |    |    |    |    |    |    |   |   |           |                |
+    +---------------------------------------------------------------------------+      |----|    |
+    |            |    |    |    |    |    |    |    |   |   |    |              |      |    |    |
+    +---------------------------------------------------------------------------+------|----|----|
+    |     |     |      |                             |     |      |    |        | |    |    |    |
+    +---------------------------------------------------------------------------+----------------+
 */
-    /*  Row:        0          1          2          3          4          5          6          7          8          9          10         11         12         13         14         15         16   */
-    [WIN_FN] = {
+
+    [WIN_FN] = { // Windows Fn overlay
+                /*  0          1          2          3          4          5          6          7          8          9          10         11         12         13         14         15         16   */
                 {   RESET,     KC_BRID,   KC_BRIU,   KC_TASK,   KC_FLXP,   RGB_VAD,   RGB_VAI,   KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_NO,     KC_TRNS,   KC_TRNS,   RGB_MOD },
                 {   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS },
                 {   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS },
@@ -93,23 +94,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 {   KC_TRNS,   KC_NO,     KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_NO,     KC_TRNS,   KC_NO,     RGB_SPI,   KC_NO   },
                 {   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_NO,     KC_NO,     KC_NO,     KC_TRNS,   KC_NO,     KC_NO,     KC_NO,     KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   RGB_SAI,   RGB_SPD,   RGB_SAD }
             },
+
 /*
-				+--------------------------------------------------------------------------+----------------+
-				|     |    |    |    |    |    |  |    |    |    |    | |    |    |    |    | |    |    |    |
-				+---------------------------------------------------------------------------+------|----|----|
- 				|     |    |    |    |    |    |    |    |    |    |    |    |    |         | |    |    |    |
- 				+---------------------------------------------------------------------------+------|----|----|
- 				|       |    |    |    |    |    |    |    |    |    |    |    |    |       | |    |    |    |
- 				+---------------------------------------------------------------------------+------|----|----|
- 				|          |    |    |    |    |    |    |    |    |    |   |   |           |                |
- 				+---------------------------------------------------------------------------+      |----|    |
- 				|            |    |    |    |    |    |    |    |   |   |    |              |      |    |    |
- 				+---------------------------------------------------------------------------+------|----|----|
- 				|     |     |      |                             |     |      |    |        | |    |    |    |
- 				+---------------------------------------------------------------------------+----------------+
+    +--------------------------------------------------------------------------+----------------+
+    |     |    |    |    |    |    |  |    |    |    |    | |    |    |    |    | |    |    |    |
+    +---------------------------------------------------------------------------+------|----|----|
+    |     |    |    |    |    |    |    |    |    |    |    |    |    |         | |    |    |    |
+    +---------------------------------------------------------------------------+------|----|----|
+    |       |    |    |    |    |    |    |    |    |    |    |    |    |       | |    |    |    |
+    +---------------------------------------------------------------------------+------|----|----|
+    |          |    |    |    |    |    |    |    |    |    |   |   |           |                |
+    +---------------------------------------------------------------------------+      |----|    |
+    |            |    |    |    |    |    |    |    |   |   |    |              |      |    |    |
+    +---------------------------------------------------------------------------+------|----|----|
+    |     |     |      |                             |     |      |    |        | |    |    |    |
+    +---------------------------------------------------------------------------+----------------+
 */
-    /*  Row:        0          1          2          3          4          5          6          7          8          9          10         11         12         13         14         15         16   */
+
     [WIN_CAPS] = {
+                /*  0          1          2          3          4          5          6          7          8          9          10         11         12         13         14         15         16   */
                 {   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_NO,     KC_TRNS,   KC_TRNS,   KC_TRNS },
                 {   KC_TRNS,   KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_F12,    KC_TRNS,   KC_TRNS,   KC_TRNS },
                 {   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_PWRD,   KC_HOME,   KC_END,    KC_NWRD,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS },
@@ -119,30 +122,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             }
 };
 
+// Manage Windows and Mac LEDs
+// - Show status of mode switch
+// - Turn LEDs off durring USB suspend
+bool mode_leds_show = true;
+bool mode_leds_windows;
+
+static void mode_leds_update(void){
+    writePin(LED_WIN_PIN, mode_leds_show && mode_leds_windows);
+    writePin(LED_MAC_PIN, mode_leds_show && !mode_leds_windows);
+}
+
+void dip_switch_update_user(uint8_t index, bool active){
+    if(index == 0) {
+        if(active) { // Mac mode
+            //layer_move(2);
+        } else { // Windows mode
+            layer_move(WIN_BASE);
+        }
+
+        // Update mode and update leds
+        mode_leds_windows = !active;
+        mode_leds_update();
+    }
+}
+
+void keyboard_pre_init_user(void) {
+    // Setup Win & Mac LED Pins as output
+    setPinOutput(LED_WIN_PIN);
+    setPinOutput(LED_MAC_PIN);
+}
+
+void keyboard_post_init_user(void) {
+    // Toggle lights off by default
+    rgblight_disable();
+}
+
+void suspend_power_down_user(void) {
+    // Turn leds off
+    mode_leds_show = false;
+    mode_leds_update();
+}
+
+void suspend_wakeup_init_user(void) {
+    mode_leds_show = true;
+    mode_leds_update();
+}
+
 // combos, change COMBO_COUNT in config.h before adding combos!
 const uint16_t PROGMEM combo_jk[] = {KC_J, KC_K, COMBO_END};
 combo_t key_combos[COMBO_COUNT] = {
     COMBO(combo_jk, KC_ESC),
 };
-
-void dip_switch_update_user(uint8_t index, bool active){
-  index = 1;
-  if(active) { //Mac mode
-    writePin(LED_NUM_LOCK_PIN, 0);
-    writePin(LED_SCROLL_LOCK_PIN, 1);
-    //layer_move(0);
-  } else { //Windows mode
-    writePin(LED_NUM_LOCK_PIN, 1);
-    writePin(LED_SCROLL_LOCK_PIN, 0);
-    layer_move(WIN_BASE);
-  }
-}
-
-void keyboard_post_init_user(void) {
-  rgblight_disable(); // lights off by default
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_matrix=true;
-  //debug_keyboard=true;
-  //debug_mouse=true;
-}
