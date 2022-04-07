0e750f213f025dfba78c56ced28eaf1ebdf60952
Simone Gaiarin <simgunz@gmail.com>
2022-04-07 15:58:21 +0200

    Add docker compose to run make in docker without login

--- keychron-c1/init.sh
+++ keychron-c1/init.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash 
 echo "Get sonix firmware..."
 [[ ! -d qmk_firmware ]] && git clone --recurse-submodules -j8 https://github.com/SonixQMK/qmk_firmware.git
-
-echo "Setting up build environment..."
-sudo systemctl start docker
-docker pull jath03/sonix_base_container
-# first time
-docker run --name sonix-qmk --interactive --tty -v "$(pwd)/keychron-c1/qmk_firmware/:/qmk" jath03/sonix_base_container
