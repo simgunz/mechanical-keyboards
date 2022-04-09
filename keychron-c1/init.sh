5d1c6a60c2040b7f015b57722a776fea1d110746
Simone Gaiarin <simgunz@gmail.com>
2022-04-09 09:05:01 +0200

    Add sonix flasher to init script

--- keychron-c1/init.sh
+++ keychron-c1/init.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash 
 QMK_FIRMWARE_REF=3f94c63
+SONIX_FLASHER_REF=v0.2.4
+
+if [[ $1 = '--help' ]]; then
+  echo "usage: init.sh [--flasher]"
+  exit 0
+fi
 
 echo "Get sonix firmware..."
 [[ -d qmk_firmware ]] || git clone https://github.com/SonixQMK/qmk_firmware.git
@@ -8,3 +14,12 @@ echo "Get sonix firmware..."
   git checkout ${QMK_FIRMWARE_REF}
   git submodule update --init --recursive --jobs 4
 )
+
+if [[ $1 = '--flasher' ]]; then
+  echo "Get sonix flasher..."
+  [[ -d sonix-flasher ]] || git clone https://github.com/SonixQMK/sonix-flasher.git
+  (
+    cd sonix-flasher
+    git checkout ${SONIX_FLASHER_REF}
+  )
+fi
