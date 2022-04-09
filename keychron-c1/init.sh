1ff6f0d2ccad68298cb392abf20cd20f3dddfbb2
Simone Gaiarin <simgunz@gmail.com>
2022-04-09 08:36:55 +0200

    Fix version of qmk_firmware and update init.sh

--- keychron-c1/init.sh
+++ keychron-c1/init.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash 
+QMK_FIRMWARE_REF=3f94c63
+
 echo "Get sonix firmware..."
-[[ ! -d qmk_firmware ]] && git clone --recurse-submodules -j8 https://github.com/SonixQMK/qmk_firmware.git
+[[ -d qmk_firmware ]] || git clone https://github.com/SonixQMK/qmk_firmware.git
+(
+  cd qmk_firmware
+  git checkout ${QMK_FIRMWARE_REF}
+  git submodule update --init --recursive --jobs 4
+)
