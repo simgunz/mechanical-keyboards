962f139dca1fecc0a4a622067725e99317d21aac
Simone Gaiarin <simgunz@gmail.com>
2025-06-10 22:09:47 +0200

    Update init to use cli flasher

--- keychron-c1/init.sh
+++ keychron-c1/init.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
 QMK_FIRMWARE_REF=sn32_master_stable
-SONIX_FLASHER_REF=v0.2.4
+SONIX_FLASHERC_REF=2.0.8
 
 if [[ $1 = '--help' ]]; then
-  echo "usage: init.sh [--flasher]"
+  echo "usage: init.sh"
   exit 0
 fi
 
 echo "Get sonix firmware..."
 [[ -d qmk_firmware ]] || git clone https://github.com/SonixQMK/qmk_firmware.git
 (
-  cd qmk_firmware
-  git checkout ${QMK_FIRMWARE_REF}
+  cd qmk_firmware || exit 1
+  git checkout "${QMK_FIRMWARE_REF}"
   git submodule update --init --recursive --jobs 4
 )
 
-echo "Link my keymap to qmk_firmware directory..."
-ln -s "$(pwd)/keymaps/simgunz/" qmk_firmware/keyboards/keychron/c1/rgb/keymaps/simgunz
+echo "Get and build SonixFlasherC (CLI)..."
+[[ -d flasher ]] || git clone https://github.com/SonixQMK/SonixFlasherC.git flasher
+(
+  cd flasher || exit 1
+  git checkout "${SONIX_FLASHERC_REF}"
+  make sonixflasher
+)
+ln -sf "$(pwd)/flasher/sonixflasher" ./sonixflasher
 
-if [[ $1 = '--flasher' ]]; then
-  echo "Get sonix flasher..."
-  [[ -d sonix-flasher ]] || git clone https://github.com/SonixQMK/sonix-flasher.git
-  (
-    cd sonix-flasher
-    git checkout ${SONIX_FLASHER_REF}
-  )
-fi
+echo "Link my keymap to qmk_firmware directory..."
+ln -sfn "$(pwd)/keymaps/simgunz" qmk_firmware/keyboards/keychron/c1/rgb/keymaps/simgunz
